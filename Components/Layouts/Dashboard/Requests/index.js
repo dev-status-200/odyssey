9f251d39a5b4caa6b529d4a7523d4ad30d76287a
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2023-07-17 18:04:25 +0500

    update

--- Components/Layouts/Dashboard/Requests/index.js
+++ Components/Layouts/Dashboard/Requests/index.js
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap';
+import readXlsxFile from 'read-excel-file'
 
 const Requests = ({sessionData}) => {
 
   useEffect(() => {
-    console.log(sessionData)
     if(sessionData.isLoggedIn==false){
       Router.push('/login')
     }
   }, [sessionData]);
 
+  const [files, setFiles] = useState();
 
   return (
     <div className='base-page-layout'>
-    
-    
-    
+        <input type="file" value={files} onChange={(e)=>{
+          setFiles(e.target.value)
+            readXlsxFile(e.target.files[0]).then((rows) => {
+              console.log(rows.splice(10, 1594))
+            })
+
+        }} />
     </div>
   )
 }
