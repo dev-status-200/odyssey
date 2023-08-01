4ee21be65a93a4790f5fdd8733275124d9a924ab
dev-status-200 <dev.status.200@gmail.com>
2023-08-01 13:06:44 +0500

    non-gl-parties update

--- Components/Layouts/Setup/Non_Gl_Parties/index.js
+++ Components/Layouts/Setup/Non_Gl_Parties/index.js
@@ -2,10 +2,10 @@ import { Row, Col, Table } from 'react-bootstrap';
 import React, { useEffect, useReducer, useState } from 'react';
 import Router from 'next/router';
 import { HistoryOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { incrementTab } from '/redux/tabs/tabSlice';
 import axios from 'axios';
-import { Select } from 'antd';
+import {Select} from 'antd'
 
 function recordsReducer(state, action){
     switch (action.type) {
@@ -46,37 +46,34 @@ function recordsReducer(state, action){
 }
 
 const initialState = {
-    records: [],
-    history:[],
-    allVendors: [],
-    searchedVendor : '',
-    // Editing Records
-    selectedRecord:{},
-    oldRecord:{},
+  records: [],
+  searchClient : '',
+  allClients : []
 };
 
-const Vendor = ({sessionData, vendorData}) => {
-
-  useEffect(()=>{ if(sessionData.isLoggedIn==false) Router.push('/login') }, [sessionData]);
+const Index = ({ clientData, sessionData}) => {
   const dispatchNew = useDispatch();
+  
+  useEffect(()=>{
+     if(sessionData.isLoggedIn==false)
+     {Router.push('/login');} 
+     setRecords(); }, 
+     [sessionData]);
 
   const [ state, dispatch ] = useReducer(recordsReducer, initialState);
-  const { records , allVendors } = state;
+  const { records, allClients } = state;
   const [searchBy , setSearchBy] = useState()
 
-  useEffect(() => {
-    setRecords();
-  }, [])
+
   
+
   const setRecords = () => {
-    dispatch({type:'toggle', fieldName:'records', payload:vendorData.result});
-    dispatch({type:'toggle', fieldName:'allVendors', payload:vendorData.result});
+    dispatch({type:'toggle', fieldName:'records', payload:clientData.result});
+    dispatch({type:'toggle', fieldName:'allClients', payload:clientData.result});
+
   }
-
-
-
-const searchVendor = () => {
-  const data = (searchBy == 'name' ? allVendors.filter((x) => x.name == state.searchedVendor) : allVendors.filter((x) => x.code == state.searchedVendor  ))
+const searchClient = () => {
+  const data = searchBy == 'name' ? allClients.filter((x) => x.name == state.searchClient) : allClients .filter((x) => x.code == state.searchClient  )
   dispatch({type:'toggle', fieldName:'records', payload:data});
 
 }
@@ -85,24 +82,11 @@ const onSearch = (event) => {
   console.log("search :", event )
 }
 
-
-  const getHistory = async(recordid,type) => {
-    dispatch({type:'toggle', fieldName:'load', payload:true});
-    dispatch({ type: 'history'})
-    await axios.get(process.env.NEXT_PUBLIC_CLIMAX_GET_HISTORY,{
-      headers:{ recordid:recordid, type:type }
-    }).then((x)=>{
-      setTimeout(async() => {
-        dispatch({type:'toggle', fieldName:'load', payload:false});
-        dispatch({type:'toggle', fieldName:'history', payload:x.data.result});
-    }, 2000);
-    })
-  }
   return (
     <div className='base-page-layout'>
     <Row>
-      <Col md={3}><h5>Vendors</h5></Col>
-      <Col md={7} style={{display:"inline-block"}}><span>Search By :</span>
+    <Col md={3}><h5>Non Gl Parties</h5></Col>
+        <Col md={7} style={{display:"inline-block"}}><span>Search By :</span>
         <Select placeholder="Search"
     onChange={(e) => setSearchBy(e)}
     style={{width:"150px", marginLeft:"5px"}}
@@ -113,26 +97,25 @@ const onSearch = (event) => {
     style={{width:"290px", marginLeft:"5px"}}
     placeholder={searchBy == 'name' ? "Type Name" : "Type Code"}
     onChange={(e) => 
-      dispatch({type:'toggle', fieldName:'searchedVendor', payload: e })}
+      dispatch({type:'toggle', fieldName:'searchClient', payload: e })}
     onSearch={onSearch}
     filterOption={(input, option) =>
       (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
     }
-    options={allVendors.map((x) => {
+    options={allClients.map((x) => {
      return (searchBy == 'name' ? {value : x.name , label : x.name} : {value : x.code , label : x.code})
     })}/>
         <button className='btn-custom right' 
-          onClick={()=> searchVendor() }>Search</button>
+          onClick={()=> searchClient() }>Search</button>
         </Col>
 
-      <Col>
+        <Col md={2}>
         <button className='btn-custom right' 
           onClick={()=>{
-            dispatchNew(incrementTab({"label":"Vendor","key":"2-8","id":"new"}));
-            Router.push(`/setup/vendor/new`);
-          }}>Create
-        </button>
-      </Col>
+            // dispatchNew(incrementTab({"label":"Client","key":"2-7","id":"new"}));
+            Router.push(`/setup/nonGlParties/new`);
+        }}>Create</button>
+        </Col>
     </Row>
     <hr className='my-2' />
     <Row style={{maxHeight:'69vh',overflowY:'auto', overflowX:'hidden'}}>
@@ -141,46 +124,33 @@ const onSearch = (event) => {
         <Table className='tableFixHead'>
         <thead>
           <tr>
+          <th>Code</th>
             <th>Name</th>
-            <th>Type</th>
             <th>Contact Persons</th>
             <th>Telephones</th>
             <th>Address</th>
-            <th>History</th>
+            {/* <th>History</th> */}
           </tr>
         </thead>
         <tbody>
-        {
-          records.map((x, index) => {
+        {records.map((x, index) => {
           return (
           <tr key={index} className='f row-hov'
             onClick={()=>{
-              dispatchNew(incrementTab({"label":"Vendor","key":"2-8","id":x.id}));
-              Router.push(`/setup/vendor/${x.id}`);
+            //   dispatchNew(incrementTab({"label":"Client","key":"2-7","id":x.id}));
+              Router.push(`/setup/nonGlParties/${x.id}`);
             }}
           >
-            <td className='blue-txt fw-7'>{x.name}</td>
-            <td>{x.types?.split(", ").map((z, i)=>{
-              return(<div key={i} className="party-types">{z}</div>)
-            })}</td>
-            <td>
-              {x.person1} {x.mobile1}<br/>
-              {x.person2} {x.mobile2}<br/>
-            </td>
-            <td>
-              {x.telephone1}<br/>
-              {x.telephone2}
-            </td>
-            <td>
-              {x.address1.slice(0,30)}<br/>
-              {x.address2.slice(0,30)}<br/>
-            </td>
-            <td>
-              Created By: <span className='blue-txt fw-5'>{x.createdBy}</span>
-              <br/>
+            <td> <span className=''>{x.code}</span></td>
+            <td> <span className='blue-txt fw-7'>{x.name}</span></td>
+            <td> {x.person1} {x.mobile1}<br/> {x.person2} {x.mobile2}<br/> </td>
+            <td> {x.telephone1}<br/>{x.telephone2}</td>
+            <td> {x.address1?.slice(0,30)}<br/> {x.address2?.slice(0,30)}<br/> </td>
+            {/* <td>
+              Created By: <span className='blue-txt fw-5'>{x.createdBy}</span> <br/>
               <span className='' style={{position:'relative', top:2}}>Load History</span>
-              <HistoryOutlined onClick={()=>getHistory(x.id,'vendor')} className='modify-edit mx-2' />
-            </td>
+              <HistoryOutlined onClick={()=>getHistory(x.id,'client')} className='modify-edit mx-2' />
+            </td> */}
           </tr>
           )
         })}
@@ -193,4 +163,4 @@ const onSearch = (event) => {
   )
 }
 
-export default Vendor
+export default Index
